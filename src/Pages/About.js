e6d15f944c9760939de6db6d80b8647be11d5c7f
ethan langford <capt.rex64@gmail.com>
2022-11-14 15:39:07 -0700

    small change
    about info moved to contact

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -15,11 +15,7 @@ const About = () => {
     <br></br>
 <h1>About page</h1>
     <br></br>
-    <div className='link'>
-    <li><a href='https://github.com/captren501'>GitHub link</a></li>
-    <li><a href='https://www.linkedin.com/in/ethan-langford-36a389236/'>linkedin</a></li>
-    <li>elangf725@west-mec.org</li>
-    </div>
+    
 
 
 </div>
--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -15,6 +15,17 @@ const Contact = () => {
 <br></br>
 <h1>Contact page</h1>
 <br></br>
+<div className='link'>
+    <li><a href='https://github.com/captren501'>GitHub link</a></li>
+    <li><a href='https://www.linkedin.com/in/ethan-langford-36a389236/'>linkedin</a></li>
+    </div>
+
+    <div>
+        <h4>gmail</h4>
+        <p>elangf725@west-mec.org: school email</p>
+        <p>  : pro email</p>
+    </div>
+
     <div>
     <h3>Phone Number</h3>
     <p>602-597-1417</p>
